3fa869e3756d858ad42915199f833fae2ff14ffb
jamesHNoble <jn428@kent.ac.uk>
2026-02-02 14:00:33 +0000

    managed to finish it by lookit it up as we learned nothing in the lectures

--- camel.py
+++ camel.py
@@ -1,3 +1,13 @@
-GetName = str(input("what is your name?"))
+def main():
+    name1 = input("what is your name?")
+    nameAdd = " "
+    for chr in name1:
+        if chr.isupper():
+            nameAdd += "_" + chr.lower()
+        else:
+            nameAdd += chr
 
-GetName()
+    print(nameAdd)
+
+main()
+
